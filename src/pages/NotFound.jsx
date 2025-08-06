ad8716fe78dda4681f42e232f3a9373b80fb99f2
Abdelrahman-Elfiky <abdelrahman.elfiky152@gmail.com>
2025-08-07 00:41:33 +0300

    initial commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="page">
+      <h2 style={{ color: 'crimson', fontSize: '2rem' }}>404</h2>
+      <p>Sorry! Page not found </p>
+    </div>
+  );
+}
+
+export default NotFound;
